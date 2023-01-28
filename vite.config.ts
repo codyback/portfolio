e639f7f16953e062da620b1bd39b0fc39563166d
Cody Backus <codyback@outlook.com>
2023-01-28 17:20:31 -0500

    testing ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
 
-import { defineConfig } from 'vitest/config'
-
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
-    },
-  },
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 	test: {
 		include: ['tests/unit/**/*.{test,spec}.{js,ts}'],
 		environment: 'jsdom',
 		globals: true,
 		setupFiles: 'tests/unit/setupTests.ts',
 		coverage: {
-      exclude: ['tests/']
-    },
+			exclude: ['tests/'],
+			// you can include other reporters, but 'json-summary' is required, json is recommended
+			reporter: ['text', 'html', 'clover', 'json-summary', 'json'],
+		},
 		deps: {
-			inline: true
+			inline: true,
 		},
 		// alias: {
 		// 	$app: path.resolve(__dirname, 'tests/unit/__mocks__/app/')
 		// },
-	}
+	},
 });
