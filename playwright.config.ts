ad4fa3cfedc6bf912ef4a4661c3819e34a2a873c
Cody Backus <codyback@outlook.com>
2023-01-29 01:22:02 -0500

    E2E Testing - Added E2E to CI for pull requests - Configuration changes for E2E - Resetting database by default before running E2E

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,10 @@ const config: PlaywrightTestConfig = {
 		command: 'npm run build && npm run preview',
 		port: 4173,
 	},
+	use: {
+		baseURL: process.env.BASE_URL || 'http://localhost:4173/',
+	},
+	reporter: process.env.CI ? 'github' : [['html'], ['list']],
 	testDir: 'tests/e2e/specs',
 };
 
