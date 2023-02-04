32455c25bb0a1101d6609258ce42359cc71f4008
Cody Backus <codyback@outlook.com>
2023-02-03 21:21:19 -0500

    feat: implement dark mode with toggle in header

--- src/lib/components/Navigation/Header.svelte
+++ src/lib/components/Navigation/Header.svelte
@@ -1,11 +1,22 @@
-<script>
+<script lang="ts">
 	import Hamburger from '@/lib/components/Navigation/Hamburger.svelte';
 	import { page } from '$app/stores';
+	import { createEventDispatcher } from 'svelte';
+	import ThemeToggle from './ThemeToggle.svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function handleDarkModeToggle() {
+		dispatch('theme', {
+			darkMode,
+		});
+	}
 
 	export let sidebar = false;
+	export let darkMode = false;
 </script>
 
-<header class="flex justify-between border-b z-20 relative">
+<header class="flex justify-between items-center border-b z-20 relative">
 	<div class="py-2">
 		<a
 			href="/"
@@ -16,41 +27,44 @@
 	</div>
 
 	<div class="py-2 text-right w-fit">
-		<nav class="py-2 tracking-wider hidden sm:inline text-neutral-800">
+		<nav
+			class="py-2 px-4 tracking-wider hidden sm:inline text-neutral-800 dark:text-neutral-300"
+		>
 			<a
 				href="/about"
-				class="px-2 py-3 rounded-md hover:bg-neutral-200 leading-10"
+				class="px-2 py-3 rounded-md hover:bg-neutral-200 dark:hover:bg-dark-700 leading-10"
 				class:underline={$page.route.id?.startsWith('/about')}
 			>
 				About
 			</a>
 			<a
 				href="/projects"
-				class="px-2 py-3 rounded-md hover:bg-neutral-200 leading-10"
+				class="px-2 py-3 rounded-md hover:bg-neutral-200 dark:hover:bg-dark-700 leading-10"
 				class:underline={$page.route.id?.startsWith('/projects')}
 			>
 				Projects
 			</a>
 			<a
 				href="/blog"
-				class="px-2 py-3 rounded-md hover:bg-neutral-200 leading-10"
+				class="px-2 py-3 rounded-md hover:bg-neutral-200 dark:hover:bg-dark-700 leading-10"
 				class:underline={$page.route.id?.startsWith('/blog')}
 			>
 				Blog
 			</a>
 			<a
 				href="/resources"
-				class="px-2 py-3 rounded-md hover:bg-neutral-200 leading-10"
+				class="px-2 py-3 rounded-md hover:bg-neutral-200 dark:hover:bg-dark-700 leading-10"
 				class:underline={$page.route.id?.startsWith('/resources')}
 			>
 				Resources
 			</a>
 		</nav>
-		<img
-			class="h-10 w-10 rounded-full bg-slate-500 hidden sm:inline-block"
-			src=""
-			alt=""
-		/>
-		<Hamburger bind:open={sidebar} />
+		<div class="flex sm:inline-block justify-center align-middle items-center">
+			<ThemeToggle
+				bind:darkMode
+				on:theme={handleDarkModeToggle}
+			/>
+			<Hamburger bind:open={sidebar} />
+		</div>
 	</div>
 </header>
---
+++ src/lib/components/SectionHeading.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let heading: string;
+</script>
+
+<div class="border-b-2 border-black dark:border-neutral-500 pb-3">
+	<h3 class="uppercase text-2xl font-bold text-right">{heading}</h3>
+</div>
+<div class="border-t-4 border-black dark:border-neutral-500 mt-0.5" />
